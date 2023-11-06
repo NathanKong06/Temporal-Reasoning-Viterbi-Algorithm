8fb026c470b6d21413d6cabbe8c73a290c9ea411
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2023-11-05 20:01:37 -0800

    Minor code refactoring and comments

--- my_solution3.py
+++ my_solution3.py
@@ -3,53 +3,43 @@ answer = []
 def read_state_weights():
     data = []
     with open("state_weights.txt","r") as f:
-        _ = f.readline()
-        state_and_weights = f.readline()
-        state_and_weights = state_and_weights.split()
-        num_states = state_and_weights[0]
-        default_weight = state_and_weights[1]
+        _ = f.readline() #Ignore first line
+        state_and_weights = f.readline() 
+        num_states,_ = state_and_weights.split()
         for _ in range(int(num_states)):
-            line = f.readline().strip()
+            line = f.readline().strip() #Remove new line symbols
             data.append(line)
-    return data,default_weight
+    return data
 
 def read_state_action_state_weights():
     data = []
     with open("state_action_state_weights.txt","r") as f:
-        _ = f.readline()
+        _ = f.readline() #Ignore first line
         infos = f.readline()
-        infos = infos.split()
-        num_triplets = infos[0]
-        num_unique_states = infos[1]
-        num_unique_actions = infos[2]
-        default_weights = infos[3]
+        num_triplets,num_unique_states,num_unique_actions,default_weights = infos.split()
         for _ in range(int(num_triplets)):
-            line = f.readline().strip()
+            line = f.readline().strip() #Remove new line symbols
             data.append(line)
     return data,num_unique_states,num_unique_actions,default_weights
 
 def read_state_observation_weights():
     data = []
     with open("state_observation_weights.txt","r") as f:
-        _ = f.readline()
+        _ = f.readline() #Ingore first line
         infos = f.readline()
-        infos = infos.split()
-        num_pairs = infos[0]
-        num_unique_states = infos[1]
-        num_unique_observations = infos[2]
-        default_weights = infos[3]
+        num_pairs,num_unique_states,num_unique_observations,default_weights = infos.split()
         for _ in range(int(num_pairs)):
-            line = f.readline().strip()
+            line = f.readline().strip() #Remove newline symbols
             data.append(line)
     return data,num_unique_states,num_unique_observations,default_weights
 
 def read_observation_actions():
     data = []
     with open("observation_actions.txt","r") as f:
-        _ = f.readline()
+        _ = f.readline() #Ignore first line
         num_pairs = f.readline()
         for _ in range(int(num_pairs)):
-            line = f.readline().strip()
+            line = f.readline().strip() #Remove newline symbols
             data.append(line)
     return data
 
@@ -58,20 +48,20 @@ def write_output(output):
         f.write("states\n")
         f.write(str(len(output)) + "\n")
         for index,_ in enumerate(output):
-            f.write("\"" + output[index] + "\"")
-            if index != len(output) -1:
+            f.write("\"" + output[index] + "\"") #Add quotation marks and value
+            if index != len(output) -1: #Do not write newline to last line
                 f.write("\n")
 
 def calculate_state_probabilities():
-    state_weights,_ = read_state_weights()
+    state_weights = read_state_weights()
     state_probabilities = {}
     total_weight = 0
     for state_and_weight in state_weights:
-        line = state_and_weight.split()
-        state = line[0].strip('"') #Remove quotation marks
-        weight = int(line[1])
+        state,weight = state_and_weight.split()
+        state = state.strip('"') #Remove quotation marks
+        weight = int(weight)
         total_weight = total_weight + weight #Track total weight
-        state_probabilities[state] = weight
+        state_probabilities[state] = weight #Set the weight for the corresponding state
 
     for state,weight in state_probabilities.items():
         state_probabilities[state] = weight/total_weight #Calculate probability 
@@ -121,9 +111,9 @@ def calculate_start_position(state_probabilities,state_observation_probabilities
     start_state = "" 
     start_observation = observation_actions[0]
     best_value = -100000
-    for state in all_states:
-        value = state_probabilities[state] * state_observation_probabilities[state][start_observation]
-        if value > best_value:
+    for state in all_states: #Calculate Start -> Every State
+        value = state_probabilities[state] * state_observation_probabilities[state][start_observation] #P(State)*P(Observation|State)
+        if value > best_value: #Track best state
             start_state = state
             best_value = value
         
@@ -131,24 +121,24 @@ def calculate_start_position(state_probabilities,state_observation_probabilities
     return start_state,best_value
 
 def calculate_hidden_states(curr_state,curr_value,curr_index,state_observation_probabilities,state_transition_probabilities,observation_actions,all_states):
-    if curr_index == len(observation_actions):
+    if curr_index == len(observation_actions): #Done iterating through observations and actions
         return 0
     next_state = ""
     action_observation = observation_actions[curr_index]
     action,observation = action_observation.split()
     best_value = -100000
-    for state in all_states:
-        value = curr_value * state_observation_probabilities[state][observation] * state_transition_probabilities[curr_state,action][state]
-        if value > best_value:
+    for state in all_states: #Calculate Current State -> Every other state
+        value = curr_value * state_observation_probabilities[state][observation] * state_transition_probabilities[curr_state,action][state] #Previous Value * P(Observation | State) * P(Next State | State, Action)
+        if value > best_value: #Track best state
             best_value = value
             next_state = state
     answer.append(next_state)
-    calculate_hidden_states(next_state,best_value,curr_index+1,state_observation_probabilities,state_transition_probabilities,observation_actions,all_states)
+    calculate_hidden_states(next_state,best_value,curr_index+1,state_observation_probabilities,state_transition_probabilities,observation_actions,all_states) #Recursively calculate every step
 
 def change_format_observation_action():
     observation_actions = read_observation_actions() #Looks like ['"Apple" "Turnaround"', '"Apple" "Forward"', '"Volcano"']
     observation_actions = [' '.join(observation_actions)] #Looks like ['"Apple" "Turnaround" "Apple" "Forward" "Volcano"']
-    observation_actions = observation_actions[0].split() #Split each word into their own elements
+    observation_actions = observation_actions[0].split() #Looks like ['"Apple"', '"Turnaround"', '"Apple"', '"Forward"', '"Volcano"']
     observation_actions = [observation_actions[0]] + [observation_actions[i] + " " + observation_actions[i+1] for i in range(1, len(observation_actions), 2)] #Looks like ['"Apple"', '"Turnaround" "Apple"', "Forward" "Volcano"']
     observation_actions = [action.replace('"', '') for action in observation_actions] #Remove quotation marks
     return observation_actions
